ad642c42912337292d58977abec73d53fb85395e
sergey <sergey@intervolga.ru>
2025-05-08 21:28:05 +0300

    Доделал установку физических групп

--- modules/data/src/services/gmsh_mesh_builder.py
+++ modules/data/src/services/gmsh_mesh_builder.py
@@ -1,12 +1,5 @@
-import math
-from collections import defaultdict
-from typing import Iterable
-
 import gmsh
-import networkx as nx
-from PySide6.QtCore import QLineF
 from PySide6.QtCore import QPointF
-from PySide6.QtGui import QPainterPath
 
 from modules.data.src.physics.turbulence_models import BoundaryConditions
 from modules.data.src.widgets.edge_item import EdgeItem
@@ -16,15 +9,14 @@ class GmshMeshBuilder:
     def __init__(self, grid_spacing: int = 1):
         self.grid_spacing = grid_spacing
         self.__boundary_lines: dict[str, list[int]] = {}
+        self.boundary_conditions: dict[int, BoundaryConditions] = {}
 
     def add_loop(self, loop: list[EdgeItem], max_element_size: float):
-        lines = []
-
         p1 = loop[0].p1
         points = [gmsh.model.geo.addPoint(p1.x() / self.grid_spacing, p1.y() / self.grid_spacing, 0, max_element_size)]
 
+        lines_groups = {}
         for edge in loop:
-
             poly = edge.path().toFillPolygon()
             for p in list(poly)[1:-1]:
                 points.append(gmsh.model.geo.addPoint(p.x() / self.grid_spacing, p.y() / self.grid_spacing, 0, max_element_size))
@@ -32,12 +24,22 @@ class GmshMeshBuilder:
             for i in range(len(points) - 1):
                 a = points[i]
                 b = points[i + 1]
-                lines.append(gmsh.model.geo.addLine(a, b))
+                if not lines_groups.get(edge.boundary_conditions.type):
+                    lines_groups[edge.boundary_conditions.type] = []
+                lines_groups[edge.boundary_conditions.type].append(gmsh.model.geo.addLine(a, b))
 
             points = points[-1:]
 
+        gmsh.model.geo.synchronize()
+        for type, lines in lines_groups.items():
+            phys_tag = gmsh.model.addPhysicalGroup(1, lines, name=type.value)
+            for edge in loop:
+                if edge.boundary_conditions.type == type:
+                    self.boundary_conditions[phys_tag] = edge.boundary_conditions
+                    break
+
         gmsh.model.geo.removeAllDuplicates()
-        return gmsh.model.geo.addCurveLoop(lines)
+        return gmsh.model.geo.addCurveLoop([line for lines in lines_groups.values() for line in lines])
 
     def build_closed_loops(self, edges: list[EdgeItem]) -> list[list[EdgeItem]]:
         """
@@ -94,7 +96,7 @@ class GmshMeshBuilder:
         for loop in loops:
             loops_tags.append(self.add_loop(loop, max_element_size))
 
-        surface_tag = gmsh.model.geo.addPlaneSurface(loops_tags)
+        gmsh.model.geo.addPlaneSurface(loops_tags)
 
         gmsh.model.geo.synchronize()
         gmsh.model.mesh.generate(2)
