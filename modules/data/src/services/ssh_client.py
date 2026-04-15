1211fedb5a2a65131de364d9a9c77d15cacd6665
sergey <sergey@intervolga.ru>
2026-04-16 00:11:28 +0300

    фикс русских букв на windows

--- modules/data/src/services/ssh_client.py
+++ modules/data/src/services/ssh_client.py
@@ -88,6 +88,8 @@ class SSHClientService:
                 stdout=subprocess.PIPE,
                 stderr=subprocess.STDOUT,
                 text=True,
+                encoding="utf-8",
+                errors="replace",
                 bufsize=1,
             )
 
